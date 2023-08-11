87f47b0a7841ed7f891113cf7e9577ff500c85ad
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-08-11 15:25:40 +0300

    create IStudentRepository

---
+++ CQRS And MediatR implementation/Repositories/IStudentRepository.cs
@@ -0,0 +1,13 @@
+﻿using CQRS_And_MediatR_implementation.Models;
+
+namespace CQRS_And_MediatR_implementation.Repositories
+{
+    public interface IStudentRepository
+    {
+        public Task<List<StudentDetails>> GetStudentListAsync();
+        public Task<StudentDetails> GetStudentByIdAsync(int Id);
+        public Task<StudentDetails> AddStudentAsync(StudentDetails studentDetails);
+        public Task<int> UpdateStudentAsync(StudentDetails studentDetails);
+        public Task<int> DeleteStudentAsync(int Id);
+    }
+}
