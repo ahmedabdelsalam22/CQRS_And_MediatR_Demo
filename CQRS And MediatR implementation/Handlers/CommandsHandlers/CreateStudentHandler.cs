24cf6b3ecab2f530c94caf8fc557a8e37ec18a9f
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-08-11 22:39:32 +0300

    CreateStudentHandler class in CommandsHandler that addStudentAsync

---
+++ CQRS And MediatR implementation/Handlers/CommandsHandlers/CreateStudentHandler.cs
@@ -0,0 +1,31 @@
+﻿using CQRS_And_MediatR_implementation.Commands;
+using CQRS_And_MediatR_implementation.Models;
+using CQRS_And_MediatR_implementation.Repositories;
+using MediatR;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+
+namespace CQRS_And_MediatR_implementation.Handlers.CommandsHandlers
+{
+    public class CreateStudentHandler : IRequestHandler<CreateStudentCommand, StudentDetails>
+    {
+        private readonly IStudentRepository _studentRepository;
+
+        public CreateStudentHandler(IStudentRepository studentRepository)
+        {
+            _studentRepository = studentRepository;
+        }
+
+        public async Task<StudentDetails> Handle(CreateStudentCommand request, CancellationToken cancellationToken)
+        {
+            var sudentDetails = new StudentDetails() 
+            {
+                StudentName = request.StudentName,
+                StudentEmail = request.StudentEmail,
+                StudentAddress = request.StudentAddress,
+                StudentAge = request.StudentAge
+            };
+
+            return await _studentRepository.AddStudentAsync(sudentDetails);
+        }
+    }
+}
