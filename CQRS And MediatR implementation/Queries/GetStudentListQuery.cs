fa956a8e4bef64fc31a572412e944e63e461d3e4
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-08-11 15:31:48 +0300

    create GetStudentListQuery class in QueriesFolder

---
+++ CQRS And MediatR implementation/Queries/GetStudentListQuery.cs
@@ -0,0 +1,9 @@
+﻿using CQRS_And_MediatR_implementation.Models;
+using MediatR;
+
+namespace CQRS_And_MediatR_implementation.Queries
+{
+    public class GetStudentListQuery : IRequest<List<StudentDetails>>
+    {
+    }
+}
