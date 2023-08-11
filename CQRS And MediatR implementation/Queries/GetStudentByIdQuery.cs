b463711e74718581ed58d9f6a40c4989f692229f
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-08-11 15:33:14 +0300

    create GetStudentByIdQuery class in Queries Folder

---
+++ CQRS And MediatR implementation/Queries/GetStudentByIdQuery.cs
@@ -0,0 +1,10 @@
+﻿using CQRS_And_MediatR_implementation.Models;
+using MediatR;
+
+namespace CQRS_And_MediatR_implementation.Queries
+{
+    public class GetStudentByIdQuery : IRequest<StudentDetails>
+    {
+        public int Id { get; set; }
+    }
+}
