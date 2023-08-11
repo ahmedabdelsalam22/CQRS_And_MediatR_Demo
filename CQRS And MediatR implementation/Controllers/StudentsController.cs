9c3e2f5373f01a1512aba702534d29244bb66230
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-08-11 23:04:41 +0300

    create StudentsController and first endPoint named GetStudentListAsync

---
+++ CQRS And MediatR implementation/Controllers/StudentsController.cs
@@ -0,0 +1,27 @@
+﻿using CQRS_And_MediatR_implementation.Models;
+using CQRS_And_MediatR_implementation.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CQRS_And_MediatR_implementation.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public StudentsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<List<StudentDetails>> GetStudentListAsync()
+        {
+            var studentDetails = await _mediator.Send(new GetStudentListQuery());
+            return studentDetails;
+        }
+    }
+}
